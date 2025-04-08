e0427be24316c407e779bf5eec60f9fc851f59ee
Sushanth N <sushanthnandeti96@gmail.com>
2025-04-07 22:02:07 -0400

    revised

--- script.js
+++ script.js
@@ -1,77 +1,79 @@
 class Player {
-    constructor (game) {
+
+    constructor(game) {
         this.game = game; 
+        this.height = 100;
         this.width = 100; 
-        this.height = 100; 
-        this.x = this.game.width * 0.5 - this.height ;
+        this.x = this.game.width * 0.5 - this.height;
         this.y = this.game.height - this.height;
-        this.speed = 5;
+        this.speed = 10;
     }
 
     draw(context) {
-        context.fillRect(this.x, this.y, this.width, this.height)
+        context.fillRect(this.x, this.y, this.width, this.height);
     }
-    // runs 60 times every second
-    update() {
-        // horizontal movement 
-        if (this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
+
+    update(){
+        // horizontal movement
+
+        if (this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed; 
         if (this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
 
-        // horizontal movement 
+        // horizontal boundaries
 
         if (this.x < 0) this.x = 0;
-        else if(this.x > this.game.width - this.width) this.x = this.game.width - this.width;
-    }   
+        else if (this.x  > this.game.width - this.width) this.x = this.game.width - this.width;
+    }
 }
-
 
 class Enemy {
 
 }
-
 
 class Projectile {
 
 }
 
 class Game {
+
     constructor(canvas) {
         this.canvas = canvas; 
-        this.width = this.canvas.width;
-        this.height = this.canvas.height;
-        this.player = new Player(this);
+        this.width = this.canvas.width; 
+        this.height = this.canvas.height; 
+        this.player  = new Player(this);
         this.keys = [];
-        
+
         // event listeners 
-        // we use to the callback arrow functions as they carry the context and also able to bind with the class variables
+
         window.addEventListener('keydown', e => {
-            if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key)
-    });
+            if (this.keys.indexOf(e.key) === -1) this.keys.push(e.key);
+            
+        })
+
         window.addEventListener('keyup', e => {
-            const index = this.keys.indexOf(e.key)
+            const index = this.keys.indexOf(e.key);
             if (index > -1) this.keys.splice(index, 1)
-    });
-}
+        })
+    }
+
     render(context) {
         this.player.draw(context);
-        this.player.update();
-        console.log(this.player.width, this.player.height);
+        this.player.update()
     }
 }
 
 window.addEventListener('load', function() {
     const canvas = document.getElementById("canvas1");
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d')
+    canvas.height = 600; 
     canvas.width = 800;
-    canvas.height = 600;
 
     let game = new Game(canvas);
-    
 
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         game.render(ctx);
-        requestAnimationFrame(animate);
+        requestAnimationFrame(animate)
     }
     animate();
 })
