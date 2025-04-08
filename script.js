9bbdb888b9b651cbb254ce001efd916fe06c6ad4
Sushanth N <sushanthnandeti96@gmail.com>
2025-04-07 22:47:30 -0400

    Projectile class initial setup done

--- script.js
+++ script.js
@@ -32,6 +32,33 @@ class Enemy {
 
 class Projectile {
 
+    constructor() {
+        this.width = 4;
+        this.height = 20; 
+        this.x;
+        this.y; 
+        this.speed;
+        this.free;
+    }
+
+    draw(context){
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    update() {
+        if (!this.free) {
+            this.y -= this.speed;
+        }
+    }  
+
+    start() {
+        this.free = false;
+    }
+
+    reset() {
+        this.free = true;
+    }
+
 }
 
 class Game {
@@ -43,6 +70,10 @@ class Game {
         this.player  = new Player(this);
         this.keys = [];
 
+        this.projectilesPool = [];
+        this.maxProjectiles = 10;
+        this.createProjectiles();
+        console.log(this.projectilesPool);
         // event listeners 
 
         window.addEventListener('keydown', e => {
@@ -59,6 +90,18 @@ class Game {
     render(context) {
         this.player.draw(context);
         this.player.update()
+    }
+
+    createProjectiles() {
+        for (let i = 0; i < this.maxProjectiles; i++ ) {
+            this.projectilesPool.push(new Projectile());
+        }
+    }
+
+    getProjectile () {
+        for (let i = 0; i < this.projectilesPool.length ; i++) {
+            if (this.projectilesPool[i].free) return this.projectilesPool[i];
+        }
     }
 }
 
